d76d4300976db951ad42fc3cda06ce4479d85e64
Jiongxiao <2609728289@qq.com>
2021-02-24 18:45:41 +0800

    获取当前时间

--- src/MyLocalTime.java
+++ src/MyLocalTime.java
@@ -1,14 +1,21 @@
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class MyLocalTime {
     public static void main(String[] args){
         LocalDate Date = LocalDate.now();
         System.out.println(Date);
 
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
         LocalTime Time = LocalTime.now();
-        System.out.println(Time);
+        String FormateTime = dateTimeFormatter.format(Time);
+        System.out.println(FormateTime);
+
+        dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         LocalDateTime DateTime = LocalDateTime.now();
-        System.out.println(DateTime);
+        String FormateDateTime = dateTimeFormatter.format(DateTime);
+        System.out.println(FormateDateTime);
     }
 }
