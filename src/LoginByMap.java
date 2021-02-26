3258935f40df9a4f8dc51a1a9b26d769b7fbba03
Jiongxiao <2609728289@qq.com>
2021-02-26 19:58:58 +0800

    模拟用户登录

---
+++ src/LoginByMap.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LoginByMap {
+    public static void main(String[] args){
+        HashMap<String,String> users = new HashMap<>();
+        users.put("admin","admin@123");
+        users.put("guess","123456");
+        users.put("my","my_666");
+        String inputuser,inputpassword;
+        String localpassword = "";
+        Scanner sc = new Scanner(System.in);
+        inputuser = sc.nextLine();
+        inputpassword = sc.nextLine();
+        for(String usertemp: users.keySet()){
+            if(usertemp.equals(inputuser)){
+                localpassword = users.get(usertemp);
+            }
+        }
+        if(localpassword == null){
+            System.out.print("您输入的账号或密码不正确");
+        }else {
+            if(localpassword.equals(inputpassword)){
+                System.out.print("登录成功");
+            }else{
+                System.out.print("您输入的账号或密码不正确");
+            }
+        }
+    }
+}
